3409205b9d8013f8c56c02145cb90a3033ddc48a
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-06-08 02:22:23 -0300

    chore: add social medias global constant

---
+++ src/config/social-media.ts
@@ -0,0 +1,5 @@
+export const SOCIAL_MEDIAS = Object.freeze({
+  linkedin: "https://www.linkedin.com/in/kaio-felipe-70730319a/",
+  github: "https://github.com/KaioFelps",
+  behance: "https://www.behance.net/kaiofelps",
+});
