ff52d137d7d19de27049f17b2ea7f0fc6c174f18
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-06-08 18:51:21 -0300

    style(progress bar): remove spinner

--- src/ui/progress-bar.tsx
+++ src/ui/progress-bar.tsx
@@ -5,7 +5,11 @@ import type { PropsWithChildren } from "react";
 
 export function ProgressBar({ children }: PropsWithChildren) {
   return (
-    <ProgressProvider color="var(--color-yellow-600)">
+    <ProgressProvider
+      color="var(--color-yellow-600)"
+      shallowRouting
+      options={{ showSpinner: false }}
+    >
       {children}
     </ProgressProvider>
   );
