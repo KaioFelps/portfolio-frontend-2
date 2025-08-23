0dc614e5b4f595fa99a7262c8a221445226a168e
KaioFelps <kaiofelipe2005@gmail.com>
2025-08-23 11:38:12 -0300

    feat: add section header component

---
+++ src/component/heading.tsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+import type { PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{
+  className?: string;
+  heading?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+}>;
+
+export function Heading({ children, className, heading: H = "h1" }: Props) {
+  return (
+    <H className={clsx("text-5xl font-bold", className && className)}>
+      {children}
+    </H>
+  );
+}
---
+++ src/component/section-header/root.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import type { PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{
+  className?: string;
+}>;
+
+export function Root({ children, className }: Props) {
+  return (
+    <header
+      className={clsx(
+        "w-full px-12 pb-6 border-b border-gray-300 dark:border-d-gray-300 flex",
+        "justify-center",
+        className && className,
+      )}
+    >
+      {children}
+    </header>
+  );
+}
