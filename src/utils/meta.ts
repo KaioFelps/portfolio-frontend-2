5bb670ca2ab091445c4f5f086c8d1be17290cbea
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-16 21:48:43 -0300

    feat(MetaUtilities): handle admin titles

--- src/utils/meta.ts
+++ src/utils/meta.ts
@@ -7,8 +7,15 @@ export abstract class MetaUtilities {
     return canonicalUrl;
   }
 
-  public static async getTitle(title: string): Promise<string> {
+  public static async getTitle(
+    title: string,
+    adminRoute = false,
+  ): Promise<string> {
     "use server";
-    return `${ServerEnv.appName} :: ${title}`;
+    const formerPart = adminRoute
+      ? `${ServerEnv.appName} :: Admin`
+      : ServerEnv.appName;
+
+    return title ? `${formerPart} :: ${title}` : formerPart;
   }
 }
