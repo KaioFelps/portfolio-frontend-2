f857fca87603fd9819cf0cc2f776a20e72c6e38d
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-19 16:12:43 -0300

    feat: add `Log` and `User` types

--- src/core/types/presented-entities/user.ts
+++ src/core/types/presented-entities/user.ts
@@ -2,3 +2,11 @@ export enum UserRole {
   admin = "ADMIN",
   editor = "EDITOR",
 }
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  role: UserRole;
+  createdAt: Date;
+};
