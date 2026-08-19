67bd6bfdf451eaef0e40c017be5653a42125656e
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-19 16:06:42 -0300

    fix(auth refresh query): add `withCredentials` option & use clean axios instance

--- src/queries/auth-queries/refresh.ts
+++ src/queries/auth-queries/refresh.ts
@@ -1,11 +1,17 @@
-import { axios } from "@/lib/axios";
+// must be a clean axios instance so that the interceptors won't
+// cause race condition
+import axios from "axios";
 import { prepareServerErrorFromAxios } from "@/lib/axios/error";
 import { mountPath } from ".";
 import type { LoginResponse } from "./login";
 
 export async function refresh(): Promise<LoginResponse> {
   try {
-    const response = await axios.post<LoginResponse>(mountPath("refresh"));
+    const response = await axios.patch<LoginResponse>(
+      mountPath("refresh"),
+      undefined,
+      { withCredentials: true },
+    );
     return response.data;
   } catch (error) {
     throw prepareServerErrorFromAxios(error);
