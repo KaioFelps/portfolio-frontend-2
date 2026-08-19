01c5e3728c6deecbfb7028d7e1b9beba6c9e48c1
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-19 16:11:58 -0300

    feat: finishes admin sidebar

--- src/ui/admin/sidebar/index.tsx
+++ src/ui/admin/sidebar/index.tsx
@@ -2,73 +2,103 @@
 
 import { BookIcon } from "@phosphor-icons/react/dist/ssr/Book";
 import { BriefcaseIcon } from "@phosphor-icons/react/dist/ssr/Briefcase";
+import { HouseIcon } from "@phosphor-icons/react/dist/ssr/House";
 import { TagIcon } from "@phosphor-icons/react/dist/ssr/Tag";
 import { UserIcon } from "@phosphor-icons/react/dist/ssr/User";
+import Link from "next/link";
+import logo from "@/assets/logo-dark-mode.svg";
+import { LogoutSidebarItem } from "./logout-item";
 import { Section } from "./section";
 
-export function AdminSidebarDynamicMenu() {
+export function AdminSidebar() {
   return (
-    <>
-      <Section
-        title="Portfólio"
-        icon={BriefcaseIcon}
-        items={[
-          {
-            href: "/admin/projetos",
-            label: "Lista de projetos",
-          },
-          {
-            href: "/admin/projetos/novo",
-            label: "Novo projeto",
-          },
-        ]}
-      />
+    <aside
+      className="from-d-backgrond absolute inset-y-0 left-0 w-[304px] border-r border-white/5 backdrop-blur-3xl"
+      style={{
+        background: `linear-gradient(rgba(16, 15, 19, 0.75), rgba(16, 15, 19, 0.75)),
+          linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+          linear-gradient(rgba(62, 64, 79, 0.4), rgba(62, 64, 79, 0.4)),
+          url("/src/assets/noise-pattern.svg") repeat center center`,
+      }}
+    >
+      <header>
+        <h1 className="px-6 py-4 border-b border-b-white/10">
+          <span className="sr-only">Kaio Felipe Website Housekeeping</span>
+          <img src={logo.src} alt="Kaio Felps" className="w-[170px]" />
+        </h1>
+      </header>
+      <nav>
+        <Link
+          href="/admin"
+          className="px-6 py-4 border-b border-white/10 flex items-center gap-3 font-bold text-base text-white hover:bg-black/10 hover:text-white/80 cursor-default active:text-white/70 transition-all duration-100"
+        >
+          <span className="text-yellow-500">
+            <HouseIcon size="20" weight="bold" />
+          </span>
+          Home
+        </Link>
+        <Section
+          title="Portfólio"
+          icon={BriefcaseIcon}
+          items={[
+            {
+              href: "/admin/projetos",
+              label: "Lista de projetos",
+            },
+            {
+              href: "/admin/projetos/novo",
+              label: "Novo projeto",
+            },
+          ]}
+        />
 
-      <Section
-        title="Blog"
-        icon={BookIcon}
-        items={[
-          {
-            href: "/admin/blog",
-            label: "Lista de posts",
-          },
-          {
-            href: "/admin/blog/novo",
-            label: "Novo post",
-          },
-        ]}
-      />
+        <Section
+          title="Blog"
+          icon={BookIcon}
+          items={[
+            {
+              href: "/admin/blog",
+              label: "Lista de posts",
+            },
+            {
+              href: "/admin/blog/novo",
+              label: "Novo post",
+            },
+          ]}
+        />
 
-      <Section
-        title="Usuários"
-        icon={UserIcon}
-        items={[
-          {
-            href: "/admin/users",
-            label: "Lista de usuários",
-          },
-          {
-            href: "/admin/users/novo",
-            label: "Novo usuário",
-            requiresAdmin: true,
-          },
-        ]}
-      />
+        <Section
+          title="Usuários"
+          icon={UserIcon}
+          items={[
+            {
+              href: "/admin/users",
+              label: "Lista de usuários",
+            },
+            {
+              href: "/admin/users/novo",
+              label: "Novo usuário",
+              requiresAdmin: true,
+            },
+          ]}
+        />
 
-      <Section
-        title="Tags"
-        icon={TagIcon}
-        items={[
-          {
-            href: "/admin/tags",
-            label: "Tags existentes",
-          },
-          {
-            href: "/admin/tags/novo",
-            label: "Criar tag",
-          },
-        ]}
-      />
-    </>
+        <Section
+          title="Tags"
+          icon={TagIcon}
+          items={[
+            {
+              href: "/admin/tags",
+              label: "Tags existentes",
+            },
+            {
+              href: "/admin/tags/novo",
+              label: "Criar tag",
+            },
+          ]}
+        />
+        <LogoutSidebarItem />
+      </nav>
+    </aside>
   );
 }
