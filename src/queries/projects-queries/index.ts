1e982f15214de08c9ccc31867b89fa4bc536f454
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-21 02:25:19 -0300

    feat: add query & hook to delete a project

--- src/queries/projects-queries/index.ts
+++ src/queries/projects-queries/index.ts
@@ -1,6 +1,7 @@
 import { generatePathFactory } from "..";
+import { deleteProject } from "./delete-project";
 import { fetchProjects } from "./fetch-projects";
 
 export const mountPath = generatePathFactory("project");
 
-export default { fetchProjects };
+export default { fetchProjects, deleteProject };
