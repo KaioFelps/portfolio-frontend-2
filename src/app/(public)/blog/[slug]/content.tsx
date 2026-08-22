e38356566f64012e44a1c087ae57ad8250e9eba2
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-22 00:28:49 -0300

    style(blogpost page): change blogpost heading

--- src/app/(public)/blog/[slug]/content.tsx
+++ src/app/(public)/blog/[slug]/content.tsx
@@ -3,6 +3,7 @@
 import { ArrowLeftIcon } from "@phosphor-icons/react/dist/ssr/ArrowLeft";
 import { ArrowRightIcon } from "@phosphor-icons/react/dist/ssr/ArrowRight";
 import clsx from "clsx";
+import Link from "next/link";
 import type { PropsWithChildren } from "react";
 import { AlertBlock } from "@/component/alert-block";
 import { Main } from "@/component/main";
@@ -14,7 +15,7 @@ import { formatDateTime } from "@/core/utils";
 function Wrapper({ children }: PropsWithChildren) {
   return (
     <Main>
-      <a
+      <Link
         href="/blog"
         className={clsx(
           "cursor-default mb-4 w-fit self-start flex items-center gap-3 px-4 py-2 rounded-full border border-gray-300 dark:border-d-gray-300 leading-none text-sm",
@@ -25,7 +26,7 @@ function Wrapper({ children }: PropsWithChildren) {
       >
         <ArrowLeftIcon size="16" weight="bold" />
         Voltar
-      </a>
+      </Link>
 
       {children}
     </Main>
@@ -50,56 +51,52 @@ export function Content({ post }: Props) {
 
   return (
     <Wrapper>
-      <header>
-        <SectionHeader.Root>
-          <SectionHeader.Heading
-            heading="h1"
-            className="text-center text-balance"
-          >
-            {post.title}
-          </SectionHeader.Heading>
-        </SectionHeader.Root>
+      <SectionHeader.Root className="flex-col gap-12 pb-24 my-24">
+        <SectionHeader.Heading
+          heading="h1"
+          className="text-center text-balance text-7xl"
+        >
+          {post.title}
+        </SectionHeader.Heading>
 
-        <div className="flex flex-col gap-2 w-full items-center mb-16 mt-6">
+        <div className="flex flex-col gap-2 w-full items-center">
           <span className="text-sm text-balance text-center mb-1 text-gray-600 dark:text-d-gray-600 wrap-break-word">
             {post.publishedAt
               ? `Criado em ${formatDateTime(post.createdAt)}. Publicado em ${formatDateTime(post.publishedAt)}.`
               : "Post ainda não publicado."}
 
-            {post.updatedAt && (
-              <>
-                <br />
-                Última edição em {post.updatedAt.toLocaleDateString("pt-Br")}.
-              </>
-            )}
+            {post.updatedAt &&
+              ` Última edição em ${formatDateTime(post.updatedAt)}.`}
           </span>
 
-          <div className="flex flex-wrap justify-center gap-1">
-            {post.tags.map((tag) => (
-              <a
-                key={`blogpost-${post.id}-tags-${tag.id}`}
-                href="/blog?queryBy=tag&query={tag.value}"
-                className={clsx(
-                  "flex",
-                  "group cursor-default text-black rounded-full px-2.5 pt-1 pb-0.5 bg-yellow-500 text-sm leading-tight transition-all",
-                  "hover:bg-yellow-600",
-                  "dark:bg-yellow-600 dark:hover:bg-yellow-500",
-                )}
-              >
-                {tag.value}
-                <div className="w-[0] group-hover:w-[calc(16px+4px)] transition-all duration-100 ease-in-out overflow-hidden">
-                  <ArrowRightIcon size="16" weight="bold" className="ml-1" />
-                </div>
-              </a>
-            ))}
-          </div>
+          {Boolean(post.tags.length) && (
+            <div className="flex flex-wrap justify-center gap-1">
+              {post.tags.map((tag) => (
+                <Link
+                  key={`blogpost-${post.id}-tags-${tag.id}`}
+                  href={`/blog?queryBy=tag&query=${tag.value}`}
+                  className={clsx(
+                    "flex",
+                    "group cursor-default text-black rounded-full px-2.5 pt-1 pb-0.5 bg-yellow-500 text-sm leading-tight transition-all",
+                    "hover:bg-yellow-600",
+                    "dark:bg-yellow-600 dark:hover:bg-yellow-500",
+                  )}
+                >
+                  {tag.value}
+                  <div className="w-0 group-hover:w-5 transition-all duration-100 ease-in-out overflow-hidden">
+                    <ArrowRightIcon size="16" weight="bold" className="ml-1" />
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
-      </header>
+      </SectionHeader.Root>
 
       <div
         id="article-body"
         className={clsx(
-          "text-container max-w-[792px] gap-4 mx-auto text-lg dark:text-d-gray-800",
+          "text-container max-w-198 gap-4 mx-auto text-lg dark:text-d-gray-800",
           "[&_:is(p,div,hr,table)]:mb-4 prose-table:max-w-full prose-table:overflow-x-scroll",
           "prose-img:max-w-full",
         )}
