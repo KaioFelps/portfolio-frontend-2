b43658d9f3302e2b8e4eab78ec0c526daa8f417c
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-22 00:11:49 -0300

    feat: add admin project creation form page

---
+++ src/app/admin/(private)/projects/new/add-new-project-link-form.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { TrashIcon } from "@phosphor-icons/react/dist/csr/Trash";
+import type { RefCallback } from "react";
+import { useForm } from "react-hook-form";
+import FloatingInput from "@/component/floating-input";
+import type { ProjectLink } from "@/core/types/presented-entities/project-link";
+
+type Props = {
+  links: ProjectLink[];
+  setLinks: (tags: ProjectLink[]) => void;
+  disabled?: boolean;
+  name?: string;
+  ref?: RefCallback<Element>;
+};
+
+export function AddNewProjectLinkForm({
+  links,
+  setLinks,
+  disabled,
+  name,
+  ref,
+}: Props) {
+  const { register, handleSubmit, reset, formState } = useForm<ProjectLink>({
+    values: {
+      title: "",
+      value: "",
+    },
+  });
+
+  const handleRemoveLink = (idOfLinkToRemove: string) => {
+    setLinks(links.filter((link) => link.value !== idOfLinkToRemove));
+  };
+
+  const handleAddNewLink = (data: ProjectLink) => {
+    setLinks([...links, data]);
+    reset();
+  };
+
+  return (
+    <form
+      name={name}
+      className="p-6 rounded-xl bg-d-backgrond/25"
+      onSubmit={handleSubmit(handleAddNewLink)}
+      ref={ref}
+    >
+      <h3 className="text-xl font-bold mb-6">Links referente ao projeto</h3>
+
+      <FloatingInput.Error error={formState.errors.title?.message} />
+      <FloatingInput.Group className="mb-3">
+        <FloatingInput.Input
+          {...register("title", {
+            required: "O título do link é obrigatório.",
+            disabled,
+          })}
+          className="w-full"
+          placeholder="https://www.kaiofelps.dev, ..."
+          type="text"
+        />
+        <FloatingInput.Label>Título do link</FloatingInput.Label>
+      </FloatingInput.Group>
+
+      <FloatingInput.Error error={formState.errors.value?.message} />
+      <FloatingInput.Group className="mb-3">
+        <FloatingInput.Input
+          {...register("value", {
+            disabled,
+            required: "O URL do link é obrigatório.",
+            validate: (value) => {
+              try {
+                new URL(value);
+                return true;
+              } catch (_) {
+                return "O URL inserido não é válido.";
+              }
+            },
+          })}
+          className="w-full"
+          name="value"
+          placeholder="https://www.kaiofelps.dev, ..."
+        />
+        <FloatingInput.Label>URL do link</FloatingInput.Label>
+      </FloatingInput.Group>
+
+      <button type="submit" className="btn ghost mb-3" disabled={disabled}>
+        Adicionar
+      </button>
+
+      {links.length > 0 && (
+        <div className="flex flex-col gap-0.5">
+          {links.map((link) => (
+            <div
+              key={`new-project-form-link-${link.value}`}
+              className="flex gap-3 justify-between w-full p-1.5 pl-3 rounded-xl bg-d-gray-300/10 hover:bg-d-gray-300/15"
+            >
+              <span className="font-medium">
+                {link.title}:{" "}
+                <a
+                  className="text-blue-500"
+                  href={link.value}
+                  target="_blank"
+                  rel="noopener"
+                >
+                  {link.value}
+                </a>
+              </span>
+              <button
+                type="button"
+                className="text-white p-1 rounded-md cursor-default bg-white/5 hover:bg-white/10 active:bg-white/15"
+                onClick={() => handleRemoveLink(link.value)}
+              >
+                <TrashIcon size="16" weight="bold" />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </form>
+  );
+}
---
+++ src/app/admin/(private)/projects/new/create-new-project-form.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import FloatingInput from "@/component/floating-input";
+import { useFetchEveryTag } from "../../hooks/use-fetch-every-tag";
+import { AddNewProjectLinkForm } from "./add-new-project-link-form";
+import { AvailableTagsSelect } from "./available-tags-select";
+import { useCreateProject } from "./hooks/use-create-project";
+import { createProjectSchema } from "./schema";
+
+export function CreateNewProjectForm() {
+  const {
+    isError: tagsCouldntLoad,
+    isPending: tagsAreLoading,
+    data: tagsData,
+  } = useFetchEveryTag();
+
+  const { formState, register, handleSubmit, control, reset, watch } = useForm({
+    resolver: zodResolver(createProjectSchema),
+    values: {
+      links: [],
+      tagsIds: [],
+      title: "",
+      topstory: "",
+    },
+  });
+
+  const {
+    isProcessing,
+    error,
+    isSuccess,
+    isError,
+    createProject,
+    reset: resetProjectCreation,
+  } = useCreateProject({
+    onSuccess: () => reset(),
+  });
+
+  const submitIsDisabled =
+    isProcessing ||
+    isError ||
+    tagsAreLoading ||
+    tagsCouldntLoad ||
+    !tagsData?.tags?.length;
+
+  useEffect(() => {
+    const subscription = watch((_values, _defaultValues) => {
+      if (isError || isSuccess) resetProjectCreation();
+    });
+
+    return () => subscription.unsubscribe();
+  }, [watch, isError, isSuccess, resetProjectCreation]);
+
+  return (
+    <>
+      {isSuccess && (
+        <span className="success alert mb-3 py-2">
+          Projeto criado com sucesso!
+        </span>
+      )}
+
+      {error &&
+        (Array.isArray(error.error) ? (
+          error.error.map((error) => (
+            <span className="danger alert mb-3 py-2">{error}</span>
+          ))
+        ) : (
+          <span className="danger alert mb-3 py-2">{error.error}</span>
+        ))}
+
+      {/* gotta be like this cuz AddNewProjectLinkForm is also a form, and forms cant be nested */}
+      <form
+        id="main-form"
+        onSubmit={handleSubmit((data) => createProject(data))}
+        onChange={() => resetProjectCreation()}
+      />
+
+      <div>
+        <FloatingInput.Error error={formState.errors.title?.message} />
+        <FloatingInput.Group className="mb-3">
+          <FloatingInput.Input
+            {...register("title")}
+            className="w-full"
+            placeholder="Nome do projeto"
+            type="text"
+            form="main-form"
+          />
+          <FloatingInput.Label>Nome do projeto</FloatingInput.Label>
+        </FloatingInput.Group>
+
+        <FloatingInput.Error error={formState.errors.topstory?.message} />
+        <FloatingInput.Group className="mb-3">
+          <FloatingInput.Input
+            {...register("topstory")}
+            className="w-full"
+            placeholder="i.imgur.com"
+            type="text"
+            form="main-form"
+          />
+          <FloatingInput.Label>Imagem de capa</FloatingInput.Label>
+        </FloatingInput.Group>
+
+        <Controller
+          control={control}
+          name="tagsIds"
+          render={({ field: { value, onChange, ...field }, fieldState }) => (
+            <>
+              <FloatingInput.Error error={fieldState.error?.message} />
+              <AvailableTagsSelect
+                {...field}
+                onSelectTagsIds={onChange}
+                selectedTagsIds={value}
+              />
+            </>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="links"
+          render={({ field: { value, onChange, ...field }, fieldState }) => (
+            <>
+              <FloatingInput.Error error={fieldState.error?.message} />
+              <AddNewProjectLinkForm
+                {...field}
+                links={value}
+                setLinks={onChange}
+              />
+            </>
+          )}
+        />
+
+        <div className="flex gap-2 mt-4">
+          <Link href="/admin/projetos" className="btn ghost">
+            Cancelar
+          </Link>
+
+          <button
+            form="main-form"
+            type="submit"
+            disabled={submitIsDisabled}
+            suppressHydrationWarning
+            className="btn default"
+          >
+            {isProcessing ? "Publicando" : "Publicar"} projeto
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
