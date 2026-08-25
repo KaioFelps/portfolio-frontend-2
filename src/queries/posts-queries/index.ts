257329e22b39e7e7814d20e3df2988c54dbc065a
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-24 23:37:00 -0300

    feat: add `publishPost` API query & hook

--- src/queries/posts-queries/index.ts
+++ src/queries/posts-queries/index.ts
@@ -1,7 +1,8 @@
 import { generatePathFactory } from "..";
 import { fetchPosts } from "./fetch-posts";
+import { publishPost } from "./publish-post";
 import { togglePostVisibility } from "./toggle-post-visibility";
 
 export const mountPath = generatePathFactory("post");
 
-export default { fetchPosts, togglePostVisibility };
+export default { fetchPosts, togglePostVisibility, publishPost };
