068908e58bdf2113cc32922a66b38ffaab1ac05e
Kaio Felps <kaiofelipe2005@gmail.com>
2026-08-31 15:40:17 -0300

    fix(admin page): add suspense boundary

--- src/app/admin/(private)/blog/edit/[slug]/page.tsx
+++ src/app/admin/(private)/blog/edit/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { AlertBlock } from "@/component/alert-block";
 import { MetaUtilities } from "@/utils/meta";
 import { EditPostForm } from "./edit-post-form";
@@ -12,20 +13,26 @@ type Props = {
 };
 
 export default async function AdminEditProjectPage({ params }: Props) {
-  const { slug } = await params;
-
   return (
     <>
       <h1 className="mb-12">Editando post</h1>
 
-      {slug ? (
-        <EditPostForm postSlug={slug} />
-      ) : (
-        <AlertBlock type="danger" className="mb-0" full>
-          O slug do blogpost não foi informado devido a um acesso inesperado a
-          essa página.
-        </AlertBlock>
-      )}
+      <Suspense fallback={null}>
+        <Content params={params} />
+      </Suspense>
     </>
+  );
+}
+
+async function Content({ params }: Props) {
+  const { slug } = await params;
+
+  if (slug) return <EditPostForm postSlug={slug} />;
+
+  return (
+    <AlertBlock type="danger" className="mb-0" full>
+      O slug do blogpost não foi informado devido a um acesso inesperado a essa
+      página.
+    </AlertBlock>
   );
 }
