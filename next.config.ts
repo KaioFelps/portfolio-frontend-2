888335e1bf60c23f29c9032e6bda9eba3bbf6520
Kaio Felps <kaiofelipe2005@gmail.com>
2026-09-02 01:53:25 -0300

    build(next config): externalize vscode oniguruma

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
 
   productionBrowserSourceMaps: false,
+  serverExternalPackages: ["vscode-oniguruma"],
 
   experimental: {
     webpackMemoryOptimizations: true,
