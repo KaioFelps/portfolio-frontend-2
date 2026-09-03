a830ef1a5e7a598fb5aadb10311fab7a62f570a4
Kaio Felps <kaiofelipe2005@gmail.com>
2026-09-02 23:56:30 -0300

    fix(blogpost card): change html elements to remove nested anchor-button

--- src/app/(public)/blog/card.tsx
+++ src/app/(public)/blog/card.tsx
@@ -14,50 +14,58 @@ export function ArticleCard({
   post: BlogPost;
   navigate: (href: string) => void;
 }) {
-  const handleCopyArticleUrl: MouseEventHandler = async (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    const url = new URL(`/blog/${post.slug}`, window.location.href);
+  const href = `/blog/${post.slug}`;
+
+  const handleCopyArticleUrl: MouseEventHandler = async (_) => {
+    const url = new URL(href, window.location.href);
     await navigator.clipboard.writeText(url.toString());
     toast.add({ description: "Link copiado!" });
   };
 
   return (
-    <Link
-      href={`/blog/${post.slug}`}
+    <div
       className={clsx(
-        "group/parent transition-all",
+        "relative group/parent transition-all",
         "cursor-default p-6 rounded-lg bg-gray-100 dark:bg-d-gray-100 border",
         "border-gray-300 dark:border-none flex gap-6 mb-2 last:mb-0 w-full",
         "hover:-translate-y-1 hover:z-10 hover:scale-[1.005] hover:shadow-lg",
       )}
     >
+      <Link
+        role="button"
+        href={href}
+        className="absolute inset-0 z-0"
+        aria-label={post.title}
+      />
+
       <img
         src={post.topstory}
-        className="min-w-66 h-32 object-cover rounded-lg max-md:hidden"
+        className="min-w-66 h-32 object-cover rounded-lg max-md:hidden relative z-10"
         alt=""
       />
 
       <div className="w-full">
         <div className="flex items-start justify-between gap-4">
-          <h3
-            className={clsx(
-              "text-[20px] font-bold relative",
-              "group-hover/parent:text-blue-500 transition-all",
-              "after:absolute after:-translate-x-1/2 after:left-1/2 after:bottom-0.5",
-              "after:h-0.5 after:w-0 after:bg-blue-500 after:transition-all",
-              "hover:after:w-full",
-            )}
-          >
-            {post.title}
-          </h3>
+          <Link role="button" href={href}>
+            <h3
+              className={clsx(
+                "text-[20px] font-bold relative",
+                "group-hover/parent:text-blue-500 transition-all",
+                "after:absolute after:-translate-x-1/2 after:left-1/2 after:bottom-0.5",
+                "after:h-0.5 after:w-0 after:bg-blue-500 after:transition-all",
+                "hover:after:w-full",
+              )}
+            >
+              {post.title}
+            </h3>
+          </Link>
           <button
-            className="text-blue-500 p-1 btn"
+            className="relative z-10 text-blue-500 p-1.5 btn not-dark:hover:bg-black/5 dark:hover:bg-white/5"
             title="Copiar link do post"
             type="button"
             onClick={handleCopyArticleUrl}
           >
-            <LinkSimpleIcon size="24" weight="bold" />
+            <LinkSimpleIcon size="20" weight="bold" />
           </button>
         </div>
 
@@ -78,9 +86,7 @@ export function ArticleCard({
             <button
               type="button"
               key={`post-${post.id}-blog-article-card-tag-${tag.id}`}
-              onClick={(event) => {
-                event.preventDefault();
-                event.stopPropagation();
+              onClick={(_event) => {
                 navigate(`/blog?q=${tag.value}&qb=tag`);
               }}
               className="px-2 py-1.5 rounded-full bg-yellow-500/10 border border-yellow-500 leading-none text-sm text-yellow-700"
@@ -90,6 +96,6 @@ export function ArticleCard({
           ))}
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
